0b52cce762c133baabb825b312c78b11fbecf3d1
Naimau-del <nemotorres.contact@gmail.com>
2024-11-22 17:30:40 +0100

    projetledebut

---
+++ tdsiteinternet/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TexteImageComponent/>
+  </div>
+</template>
+
+<script>
+import TexteImageComponent from './components/TexteImageComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TexteImageComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tdsiteinternet/src/components/TexteImageComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Mon Composant</h1>
+        <input type="image" src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Submit" width="48" height="48">
+        <input type="text" v-model="inputValue" placeholder="Tapez quelque chose..."/>
+        <p v-show="inputValue">Vous avez tapé : {{ inputValue }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello World',
+            inputValue: ''
+        };
+}
+};
+</script>
+
+<style scoped>
+</style>
